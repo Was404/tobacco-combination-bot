0bf7fe4f870bf97eea45d201b9d9cab192f4c101
Danya <vladimirovichdanil@bk.ru>
2023-08-10 12:25:38 +0300

    processing the output of the response

--- backend/112.py
+++ backend/112.py
@@ -1,46 +1,28 @@
 import pandas as pd
-#import string
-sovpad_vkusi = pd.read_excel('data\TABAKI2.xlsx')
-black_bern = pd.read_excel('data\BlackBern.xlsx')
 
-print("Кол-во столбцов ", black_bern.shape[1])
-print("Кол-во столбцов TABAKI2", sovpad_vkusi.shape[1])
+sovpad_vkusi = pd.read_excel('backend\data\TABAKI2.xlsx')
+black_bern = pd.read_excel('backend\data\BlackBern.xlsx')
+black_bern.drop_duplicates(keep = "last", inplace = True)
 
-msg = "Irish cream, Basillic, Haribon, Pistachio ice snow"
-#extracted_words = str.maketrans("", "", string.punctuation)
-#msg = msg.translate(extracted_words)
-words_list = msg.split(", ")
-print(words_list)
-result = []
+def result(msg):
 
-for i in range(len(words_list[:-1:])):
-    first = words_list[i]
-    second = words_list[i+1]
-    print("INPUT:", first, second)
-    name_of_col1 = black_bern.columns[black_bern.isin([first]).any()][0]
-    name_of_col2 = black_bern.columns[black_bern.isin([second]).any()][0]
-    print("OUTPUT:", name_of_col1, name_of_col2)
+    words_list = msg.split(", ")
+    print(words_list)
+    result = []
+    name_of_col_list = []
+    count = 0
+    print("ｔｈｅ ｍｉｘｅｒ ｈａｓ ｓｔａｒｔｅｄ")
+    for i in range(len(words_list[:-1:])):
 
-    idx = sovpad_vkusi[sovpad_vkusi.iloc[:, 0] == name_of_col1].index[0]
-    print("IDX:", idx)   
-    col_name = sovpad_vkusi.columns[sovpad_vkusi.columns.str.contains(name_of_col2)][0] 
-    print("COLNAME", col_name)
+            first = words_list[i]
+            second = words_list[i+1]
+            print("INPUT:", first, "|", second)
 
-    result.append(sovpad_vkusi.loc[idx, col_name])
-    print(result)
-
-if len(words_list) > 2:
-        print("!Start mathing last!")
-        last = words_list[-1]
-        
-        for i in reversed(range(len(words_list) -1)):
-            if i == len(words_list) - 2:
-                continue
-            pepa = words_list[i]
-            print("INPUT:", last, pepa)
-
-            name_of_col1 = black_bern.columns[black_bern.isin([last]).any()][0]
-            name_of_col2 = black_bern.columns[black_bern.isin([pepa]).any()][0]
+            name_of_col1 = black_bern.columns[black_bern.isin([first]).any()][0]
+            while count == 1:
+                name_of_col_list.append(name_of_col1)
+            name_of_col2 = black_bern.columns[black_bern.isin([second]).any()][0]
+            name_of_col_list.append(name_of_col2)
             print("OUTPUT:", name_of_col1, name_of_col2)
 
             idx = sovpad_vkusi[sovpad_vkusi.iloc[:, 0] == name_of_col1].index[0]
@@ -49,5 +31,32 @@ if len(words_list) > 2:
             print("COLNAME", col_name)
 
             result.append(sovpad_vkusi.loc[idx, col_name])
+            count +=1
 
-print(result)
+    if len(words_list) > 2:
+        print("!Start mathing last!")
+        last = words_list[-1]
+            
+        for i in reversed(range(len(words_list) -1)):
+            if i == len(words_list) - 2:
+                    continue
+            pepa = words_list[i]
+            print("INPUT:", last, "|", pepa)
+
+            name_of_col1 = black_bern.columns[black_bern.isin([last]).any()][0]
+            name_of_col2 = black_bern.columns[black_bern.isin([pepa]).any()][0]
+            print("OUTPUT:", name_of_col1, name_of_col2)
+
+            idx = sovpad_vkusi[sovpad_vkusi.iloc[:, 0] == name_of_col1].index[0]
+            print("IDX:", idx)   
+            col_name = sovpad_vkusi.columns[sovpad_vkusi.columns.str.contains(name_of_col2)][0]
+            print("COLNAME", col_name)
+
+            result.append(sovpad_vkusi.loc[idx, col_name])
+        print("ｗｏｒｋ ｉｓ ｏｖｅｒ")
+        print(name_of_col_list)
+        
+        return result
+
+if __name__ == "__main__":
+    print(result("Basillic, Mirinda, Cheesecake"))
