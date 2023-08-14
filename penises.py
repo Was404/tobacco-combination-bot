6862f915608c66538cc6f335ac724c4a180977c0
Was403 <vladimirovichdanil@bk.ru>
2023-08-14 22:55:18 +0300

    fixed keyboard

--- penises.py
+++ penises.py
@@ -6,3 +6,11 @@
 #async def send_adress(message: types.Message):
 #   await bot.send_location(chat_id = message.from_user.id, latitude = 55, longitude = 74)
 # message.capitalize()
+@dp.message_handler()
+async def interception(message: types.Message):
+    if message.text.count(' ') >= 1:
+        await message.answer(result(message.text))
+    else:
+        await message.answer(text = COUNT_ERROR, parse_mode="HTML")
+        await bot.send_sticker(message.from_user.id, sticker="CAACAgIAAxkBAAEJ9exk0oa87s2dqdLDTO0j6_g3tyYDbgAC1AsAAg74eUlJg1T3YVm93jAE")
+
